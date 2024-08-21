a64a1f57dcc5f1dfcf07da39c1d58b92df687839
Radithya <radithyaariputra@gmail.com>
2024-08-21 20:33:05 +0700

    update server.js

--- server.js
+++ server.js
@@ -579,10 +579,6 @@ app.post('/api/penelitian/update', async (req, res) => {
 app.post('/api/magang/update', async (req, res) => {
     console.log("Request body:", req.body); // Debugging log
     const { id, nomorSurat, statusAjuan } = req.body;
-
-    console.log("Parsed ID:", id);
-    console.log("Parsed nomorSurat:", nomorSurat);
-    console.log("Parsed statusAjuan:", statusAjuan)
     
     if (!id || (!nomorSurat && !statusAjuan)) {
         return res.status(400).json({ message: 'ID dan setidaknya satu dari Nomor Surat atau Status diperlukan' });
@@ -611,6 +607,9 @@ app.post('/api/magang/update', async (req, res) => {
         console.error('Error updating document:', err);
         res.status(500).json({ message: 'Kesalahan Server Internal' });
     }
+    console.log("Parsed ID:", id);
+    console.log("Parsed nomorSurat:", nomorSurat);
+    console.log("Parsed statusAjuan:", statusAjuan)
 });
 
 app.get('/', (req, res) => {
