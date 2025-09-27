f7c0ea2a5b5d112a31a1baab076c8e8c0ff6f199
Radithya <radithyaariputra@gmail.com>
2025-09-28 04:06:31 +0700

    updated server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      private_key: process.env.PRIVATE_KEY.split(String.raw`\n`).join('\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
